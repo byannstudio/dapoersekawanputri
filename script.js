0c284f625026f8124ec1b732303c790e181b2209
anpiu23 <annisafrrdaus@gmail.com>
2025-07-04 10:16:07 +0700

    Revert "Merge branch 'main' of https://github.com/byannstudio/dapoersekawanputri"
    This reverts commit d5f9ecd6f0ced6ed0085be0754cac05e3d7f00c4, reversing
    changes made to 1a9fe366b014a48d9b8141792b78ee82668250ea.

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-function pesan(namaProduk) {
-  const nomor = "6285878299285"; // Ganti dengan nomor WhatsApp kamu
-  const pesan = `Halo, saya ingin memesan produk: ${namaProduk}`;
-  const url = `https://wa.me/${nomor}?text=${encodeURIComponent(pesan)}`;
-  window.open(url, '_blank');
-}
+// Untuk kebutuhan tambahan interaktif di masa depan
+console.log("Website Dapoer Sekawan Putri siap digunakan!");
